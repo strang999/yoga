d47adeb95920a7b26dc2380b2d38f8664bda2d5a
Anton Shumeiko <49491454+strang999@users.noreply.github.com>
2020-02-04 13:00:24 +0200

    YOGA:ADD

--- src/js/parts/timer.js
+++ src/js/parts/timer.js
@@ -1,5 +1,5 @@
 function timer() {
-    let deadline = '2020-02-01';
+    let deadline = '2020-02-21';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
